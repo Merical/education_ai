b09dd5858074ff271be76b3b4be37e111fe897f3
Merical <lch94102@163.com>
2019-03-07 16:42:39 +0800

    first commit

--- requirement/ubuntu16_04_install_basic.sh
+++ requirement/ubuntu16_04_install_basic.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd ~
-wget https://github.com/Merical/education_ai/blob/master/requirement/ubuntu16.04_sources.list
+wget https://github.com/Merical/education_ai/blob/master/requirement/ubuntu16_04_sources.list
 echo update sources.list
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
-sudo cp ./ubuntu16.04_sources.list /etc/apt/sources.list
+sudo cp ./ubuntu16_04_sources.list /etc/apt/sources.list
 sudo apt-get update
 sleep 3
 sudo apt-get install build-essential git python-dev python-pip python-numpy -y
